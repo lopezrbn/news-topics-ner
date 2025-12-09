e74952b188c93eb4ed15d1eb723c9707e6da8818
Rubén López <lopezrbn@gmail.com>
2025-12-09 13:31:17 +0100

    Refactor PostgreSQL initialization script to organize user creation, database creation, and permission grants

--- docker/postgres-init.sql
+++ docker/postgres-init.sql
@@ -1,11 +1,25 @@
-    -- news_nlp (proyecto)
+-- 1) Crear usuarios
 CREATE USER news_nlp_user WITH PASSWORD 'news_nlp_password';
-CREATE DATABASE news_nlp OWNER news_nlp_user;
+CREATE USER mlflow_user   WITH PASSWORD 'mlflow_password';
+CREATE USER airflow_user  WITH PASSWORD 'airflow_password';
 
--- mlflow
-CREATE USER mlflow_user WITH PASSWORD 'mlflow_password';
-CREATE DATABASE mlflow_db OWNER mlflow_user;
-
--- airflow
-CREATE USER airflow_user WITH PASSWORD 'airflow_password';
+-- 2) Crear bases de datos
+CREATE DATABASE news_nlp   OWNER news_nlp_user;
+CREATE DATABASE mlflow_db  OWNER mlflow_user;
 CREATE DATABASE airflow_db OWNER airflow_user;
+
+-- 3) Dar permisos completos y cambiar el owner del schema public
+--    MLflow
+\connect mlflow_db
+GRANT ALL ON SCHEMA public TO mlflow_user;
+ALTER SCHEMA public OWNER TO mlflow_user;
+
+--    Airflow
+\connect airflow_db
+GRANT ALL ON SCHEMA public TO airflow_user;
+ALTER SCHEMA public OWNER TO airflow_user;
+
+--    Negocio
+\connect news_nlp
+GRANT ALL ON SCHEMA public TO news_nlp_user;
+ALTER SCHEMA public OWNER TO news_nlp_user;
