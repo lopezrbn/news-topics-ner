b03c5f2a4db64fa0e46c26448327d464118e7de5
Rubén López <lopezrbn@gmail.com>
2025-12-09 13:22:33 +0100

    Refactor PostgreSQL initialization and update environment variables in docker-compose

--- docker/postgres-init.sql
+++ docker/postgres-init.sql
@@ -1,14 +1,11 @@
     -- news_nlp (proyecto)
-CREATE DATABASE news_nlp;
-CREATE USER news_nlp WITH PASSWORD 'news_nlp_password';
-GRANT ALL PRIVILEGES ON DATABASE news_nlp TO news_nlp;
-
--- airflow
-CREATE DATABASE airflow_db;
-CREATE USER airflow WITH PASSWORD 'airflow_password';
-GRANT ALL PRIVILEGES ON DATABASE airflow_db TO airflow;
+CREATE USER news_nlp_user WITH PASSWORD 'news_nlp_password';
+CREATE DATABASE news_nlp OWNER news_nlp_user;
 
 -- mlflow
-CREATE DATABASE mlflow_db;
-CREATE USER mlflow WITH PASSWORD 'mlflow_password';
-GRANT ALL PRIVILEGES ON DATABASE mlflow_db TO mlflow;
+CREATE USER mlflow_user WITH PASSWORD 'mlflow_password';
+CREATE DATABASE mlflow_db OWNER mlflow_user;
+
+-- airflow
+CREATE USER airflow_user WITH PASSWORD 'airflow_password';
+CREATE DATABASE airflow_db OWNER airflow_user;
